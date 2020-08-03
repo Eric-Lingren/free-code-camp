d6fba4dd0550dd200e05fd86a488f1a0d900d9e0
Eric Lingren <ericlingren@gmail.com>
2020-08-03 17:33:12 -0600

    code cleanup

--- scientific-computing-with-python/projects/arithmetic-formatter/arithmetic_arranger.py
+++ scientific-computing-with-python/projects/arithmetic-formatter/arithmetic_arranger.py
@@ -1,4 +1,43 @@
 
+def arithmetic_arranger(problems):
+    error = None
+   
+    for problem in problems:
+        valid_problem_opperator = check_problem_opperators(problem)
+        if valid_problem_opperator is False:
+            error = "Error: Operator must be '+' or '-'."
+            break
+        valid_problem_integer = check_opperand_integers(problem)
+        if valid_problem_integer is False:
+            error = "Error: Numbers must only contain digits."
+            break
+        valid_opperand_length = check_opperand_length(problem)
+        if valid_opperand_length is False:
+            error = "Error: Numbers cannot be more than four digits."
+            break
+
+    valid_problem_quantity = check_problem_quantity(problems)
+    if valid_problem_quantity is False:
+        error = 'Error: Too many problems.'
+
+    arranged_problems = ''
+
+    if error is not None:
+        return error
+    if error is None:
+        problem_spacing = 4
+        line1 = ''
+        line2 = ''
+        line3 = ''
+        for problem in problems:
+            line1 = ( line1 + build_print_output_line_1(problem) + (' ' * problem_spacing) )
+            line2 = ( line2 + build_print_output_line_2(problem) + (' ' * problem_spacing) )
+            line3 = ( line3 + build_print_output_line_3(problem) + (' ' * problem_spacing) )
+
+        arranged_problems = line1 + '\n' + line2 + '\n'+ line3  
+        return arranged_problems 
+
+        
 def check_problem_quantity(problems):
     problem_length = len(problems)
     if problem_length <= 4:
@@ -92,44 +131,3 @@ def inject_line2_whitespace(max_size, opperand, opperator):
     difference = max_size - 1 - len(opperand)
     spaces = ' ' * difference
     return opperator + spaces + opperand
-
-
-def arithmetic_arranger(problems):
-
-    error = None
-   
-    for problem in problems:
-        valid_problem_opperator = check_problem_opperators(problem)
-        if valid_problem_opperator is False:
-            error = "Error: Operator must be '+' or '-'."
-            break
-        valid_problem_integer = check_opperand_integers(problem)
-        if valid_problem_integer is False:
-            error = "Error: Numbers must only contain digits."
-            break
-        valid_opperand_length = check_opperand_length(problem)
-        if valid_opperand_length is False:
-            error = "Error: Numbers cannot be more than four digits."
-            break
-
-    valid_problem_quantity = check_problem_quantity(problems)
-    if valid_problem_quantity is False:
-        error = 'Error: Too many problems.'
-
-    arranged_problems = ''
-
-    if error is not None:
-        return error
-    if error is None:
-        problem_spacing = 4
-        line1 = ''
-        line2 = ''
-        line3 = ''
-        for problem in problems:
-            line1 = ( line1 + build_print_output_line_1(problem) + (' ' * problem_spacing) )
-            line2 = ( line2 + build_print_output_line_2(problem) + (' ' * problem_spacing) )
-            line3 = ( line3 + build_print_output_line_3(problem) + (' ' * problem_spacing) )
-
-        arranged_problems = line1 + '\n' + line2 + '\n'+ line3  
-        # print(arranged_problems)
-        return arranged_problems 
